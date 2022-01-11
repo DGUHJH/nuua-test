e5d5d439d083629d77c4f3c0be1283534e4b38ab
DGUHJH <ghkdwogud852@gmail.com>
2022-01-11 15:27:29 +0900

    Update : MyPage Layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
+import { Badge } from '@mui/material';
 import Header from 'components/Header/pc';
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import { useState } from 'react';
 import * as Styled from 'styles/styled';
 
 const Home: NextPage = () => {
@@ -16,10 +18,102 @@ const Home: NextPage = () => {
   );
 };
 
+const testInfoNumberData = [
+  {
+    label: '알림',
+    number: 5,
+    isBadgeInVisible: false,
+  },
+  {
+    label: '찜하기',
+    number: 32,
+    isBadgeInVisible: true,
+  },
+  {
+    label: '예약',
+    number: 2,
+    isBadgeInVisible: true,
+  },
+  {
+    label: '문의내역',
+    number: 3,
+    isBadgeInVisible: true,
+  },
+];
+
+const infoManageMenuList = [
+  {
+    label: '나의 계정',
+  },
+  {
+    label: '탑승자 정보',
+  },
+  {
+    label: '결제수단',
+  },
+];
+
 const PC = () => {
+  const [isBadgeInVisible, setIsBadgeInVisible] = useState<boolean>(false);
   return (
     <Styled.Root>
       <Header />
+      <Styled.Body>
+        <Styled.BodyContainer>
+          <Styled.TitleTypo>안녕하세요. TripAI 회원님</Styled.TitleTypo>
+          <Styled.InfoManageButton>
+            <Styled.InfoManageButtonTypo>정보관리</Styled.InfoManageButtonTypo>
+          </Styled.InfoManageButton>
+          <Styled.InfoNumberContainer>
+            {testInfoNumberData.map((numberData, index) => (
+              <Styled.InfoNumberWrapper key={`info_number_wrapper_${index}`}>
+                <Styled.InfoNumberTitleTypo>
+                  {numberData.label}
+                </Styled.InfoNumberTitleTypo>
+                <Badge
+                  color="secondary"
+                  variant="dot"
+                  invisible={numberData.isBadgeInVisible}
+                >
+                  <Styled.InfoNumberTypo>
+                    {numberData.number}
+                  </Styled.InfoNumberTypo>
+                </Badge>
+              </Styled.InfoNumberWrapper>
+            ))}
+          </Styled.InfoNumberContainer>
+          <Styled.InfoManageContainer>
+            <Styled.InfoManageTitleTypo>정보관리</Styled.InfoManageTitleTypo>
+            <Styled.InfoManageMenuContainer>
+              {infoManageMenuList.map((manageMenu, index) => (
+                <Styled.InfoManageMenuTypo
+                  key={`info_manage_menu_typo_${index}`}
+                >
+                  {manageMenu.label}
+                </Styled.InfoManageMenuTypo>
+              ))}
+            </Styled.InfoManageMenuContainer>
+            <Styled.InfoManageCreditContainer>
+              <Styled.InfoManageCreditEditButtonContainer>
+                <Styled.InfoManageCreditEditButton>
+                  <Styled.InfoManageCreditEditButtonTypo>
+                    편집
+                  </Styled.InfoManageCreditEditButtonTypo>
+                </Styled.InfoManageCreditEditButton>
+              </Styled.InfoManageCreditEditButtonContainer>
+              <Styled.InfoManageCreditCardContainer />
+              <Styled.InfoManageCreditAddContainer>
+                <Styled.InfoManageCreditAddIconWrapper>
+                  <Styled.InfoManageCreditAddIcon />
+                </Styled.InfoManageCreditAddIconWrapper>
+                <Styled.InfoManageCreditAddTypo>
+                  결제수단 추가
+                </Styled.InfoManageCreditAddTypo>
+              </Styled.InfoManageCreditAddContainer>
+            </Styled.InfoManageCreditContainer>
+          </Styled.InfoManageContainer>
+        </Styled.BodyContainer>
+      </Styled.Body>
     </Styled.Root>
   );
 };
--- src/styles/styled.ts
+++ src/styles/styled.ts
@@ -1,10 +1,222 @@
-import { Box } from '@mui/material';
+import { Add } from '@mui/icons-material';
+import { Box, Button, Typography } from '@mui/material';
 import styled from 'styled-components';
 
 export const Root = styled(Box)`
   width: 100%;
-  height: 100px;
   display: flex;
   flex-direction: column;
   align-items: center;
+`;
+
+export const Body = styled(Box)`
+  width: 100%;
+  min-height: calc(100vh - 100px);
+  background: #dddee5;
+  display: flex;
+  justify-content: center;
+  padding-bottom: 200px;
+`;
+
+export const BodyContainer = styled(Box)`
+  width: 1224px;
+  height: 1049px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: relative;
+  margin-top: 81px;
+`;
+
+export const TitleTypo = styled(Typography)`
+  &&& {
+    font-family: Poppins-Bold;
+    font-weight: 700;
+    font-size: 30px;
+    color: #111d2d;
+  }
+`;
+
+export const InfoManageButton = styled(Button)`
+  &&& {
+    width: 65px;
+    height: 32px;
+    background: #fff;
+    border: 1px #dddee5 solid;
+    border-radius: 4px;
+    margin-top: 16px;
+  }
+`;
+
+export const InfoManageButtonTypo = styled(Typography)`
+  &&& {
+    font-family: Poppins-Bold;
+    font-weight: 700;
+    font-size: 11px;
+    color: #777e97;
+  }
+`;
+
+export const InfoNumberContainer = styled(Box)`
+  width: 640px;
+  height: 68px;
+  display: flex;
+  justify-content: space-between;
+  margin-top: 40px;
+`;
+
+export const InfoNumberWrapper = styled(Box)`
+  width: 160px;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  box-sizing: border-box;
+  border-left: 1px #0002 solid;
+  &:first-child {
+    border-left: 0;
+  }
+`;
+
+export const InfoNumberTitleTypo = styled(Typography)`
+  &&& {
+    font-family: Poppins-Regular;
+    font-weight: 400;
+    font-size: 15px;
+    color: #777e97;
+    line-height: 18px;
+  }
+`;
+
+export const InfoNumberTypo = styled(Typography)`
+  &&& {
+    font-family: Poppins-Bold;
+    font-weight: 700;
+    font-size: 30px;
+    color: #333849;
+    line-height: 42px;
+  }
+`;
+
+export const InfoManageContainer = styled(Box)`
+  width: 1224px;
+  height: 787px;
+  background: #fff;
+  display: flex;
+  flex-direction: column;
+  padding: 64px;
+  box-sizing: border-box;
+  border-radius: 8px;
+  margin-top: 64px;
+`;
+
+export const InfoManageTitleTypo = styled(Typography)`
+  &&& {
+    font-family: Poppins-Bold;
+    font-weight: 700;
+    font-size: 25px;
+    color: #333849;
+    line-height: 35px;
+  }
+`;
+
+export const InfoManageMenuContainer = styled(Box)`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  margin-top: 64px;
+`;
+
+export const InfoManageMenuTypo = styled(Typography)`
+  &&& {
+    font-family: Poppins-Bold;
+    font-weight: 700;
+    font-size: 15px;
+    color: #aab0bc;
+    line-height: 18px;
+    padding: 0 16px;
+    box-sizing: border-box;
+    border-right: 1px #dddee5 solid;
+  }
+  &:first-child {
+    &&& {
+      padding-left: 0;
+    }
+  }
+  &:last-child {
+    &&& {
+      border-right: 0;
+      color: #333849;
+    }
+  }
+`;
+
+export const InfoManageCreditContainer = styled(Box)`
+  width: 1096px;
+  height: 496px;
+  display: flex;
+  flex-direction: column;
+  margin-top: 24px;
+`;
+
+export const InfoManageCreditEditButtonContainer = styled(Box)`
+  width: 100%;
+  display: flex;
+  justify-content: flex-end;
+`;
+
+export const InfoManageCreditEditButton = styled(Button)`
+  width: 60px;
+  height: 32px;
+  background: #fff;
+  border: 1px #dddee5 solid;
+  border-radius: 4px;
+`;
+
+export const InfoManageCreditEditButtonTypo = styled(Typography)`
+  &&& {
+    font-family: Poppins-Bold;
+    font-weight: 700;
+    font-size: 11px;
+    color: #777e97;
+  }
+`;
+
+export const InfoManageCreditCardContainer = styled(Box)`
+  width: 1096px;
+  height: 360px;
+  background: #f6f7fb;
+  border: 1px #e9ebee solid;
+  border-radius: 8px;
+  margin-top: 24px;
+`;
+
+export const InfoManageCreditAddContainer = styled(Box)`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 40px;
+`;
+
+export const InfoManageCreditAddIconWrapper = styled(Box)`
+  width: 40px;
+  height: 40px;
+  position: relative;
+`;
+
+export const InfoManageCreditAddIcon = styled(Add)`
+  font-size: 40px;
+  color: #ccced8;
+`;
+
+export const InfoManageCreditAddTypo = styled(Typography)`
+  &&& {
+    font-family: Poppins-Bold;
+    font-weight: 700;
+    font-size: 20px;
+    color: #555e70;
+    margin-left: 8px;
+  }
 `;
